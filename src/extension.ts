960dad210836c2fd6889adbed5a649259e71560c
frn.fnts <frn.fnts@gmail.com>
2025-04-10 21:46:49 +0900

    init

---
+++ src/extension.ts
@@ -0,0 +1,143 @@
+import * as vscode from "vscode";
+type Direction = -1 | 1;
+
+export function activate(context: vscode.ExtensionContext) {
+  const moveToNextEdge = vscode.commands.registerCommand(
+    "vscode-edgemotion.MoveToNextEdge",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      const position = editor.selection.active;
+      const nextLine = findEdge(editor, position.line, 1); // Forward direction
+      if (nextLine !== null) {
+        const newPosition = position.with(nextLine, editor.selection.active.character);
+        editor.selection = new vscode.Selection(newPosition, newPosition);
+        editor.revealRange(new vscode.Range(newPosition, newPosition));
+      }
+    }
+  );
+
+  const moveToPreviousEdge = vscode.commands.registerCommand(
+    "vscode-edgemotion.MoveToPreviousEdge",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      const position = editor.selection.active;
+      const previousLine = findEdge(editor, position.line, -1); // Backward direction
+      if (previousLine !== null) {
+        const newPosition = position.with(previousLine, editor.selection.active.character);
+        editor.selection = new vscode.Selection(newPosition, newPosition);
+        editor.revealRange(new vscode.Range(newPosition, newPosition));
+      }
+    }
+  );
+
+  context.subscriptions.push(moveToNextEdge, moveToPreviousEdge);
+
+}
+export function deactivate() {}
+
+
+export function findEdge(
+  editor: vscode.TextEditor,
+  startLine: number,
+  direction: Direction
+): number | null {
+  const document = editor.document;
+  const lineCount = document.lineCount;
+  const island_start = island(
+    editor,
+    startLine,
+    editor.selection.active.character
+  );
+  const island_next = island(
+    editor,
+    startLine + direction,
+    editor.selection.active.character
+  );
+  const should_move_to_land = !(island_start && island_next);
+  let line = startLine;
+
+  if (should_move_to_land) {
+    if (island_start && !island_next) {
+      line += direction;
+    }
+    while (
+      line >= 0 &&
+      line < lineCount &&
+      !island(editor, line, editor.selection.active.character)
+    ) {
+      line += direction;
+    }
+  } else {
+    while (
+      line >= 0 &&
+      line < lineCount &&
+      island(editor, line, editor.selection.active.character)
+    ) {
+      line += direction;
+    }
+    line -= direction;
+  }
+
+  if (line >= 0 && line < lineCount) {
+    return line;
+  }
+  line += direction;
+
+  return null; // Edge not found
+}
+
+// This function checks if the character at the given line and column is an island
+// 1. If the character is not whitespace, it is an island
+// 2. If the character is whitespace, it checks the characters before and after it
+// 3. If both characters are not whitespace, it is an island
+export function island(
+  editor: vscode.TextEditor,
+  line: number,
+  vcol: number
+): boolean {
+  const document = editor.document;
+  if (line < 0 || line >= document.lineCount) {
+    return false;
+  }
+
+  const text = document.lineAt(line).text;
+  if (vcol < 0 || vcol >= text.length) {
+    return false;
+  }
+
+  const [left, center, right] = getCharsAround(text, vcol);
+  if (!center) {
+    return false;
+  }
+  if (!isWhite(center)) {
+    return true;
+  }
+  if (!left || !right) {
+    return false;
+  }
+  return !isWhite(left) && !isWhite(right);
+}
+
+function isWhite(char: string): boolean {
+  return char === " " || char === "\t";
+}
+
+// This function is used to get the characters around the cursor position
+// ex) if the line is "hello world" and the cursor is at index 4, it will return "o"
+export function getCharsAround(line: string, vcol: number): string[] {
+  const pattern = new RegExp(`^(.{${vcol}})(.)(.?)`);
+  // group 0: everything before the cursor
+  // group 1: the character at the cursor
+  // group 2: the character after the cursor
+  const match = line.match(pattern);
+  if (!match) {
+    return [];
+  }
+  return [match[1][vcol - 1] ?? "", match[2], match[3]];
+}
---
+++ src/test/extension.test.ts
@@ -0,0 +1,295 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import * as edgeMotion from '../extension';
+
+
+const content = `0
+1
+  2
+    3
+  4
+5
+
+7       8
+  8    7 9
+9       8
+  10 5  8   x
+11
+
+13
+14
+15
+
+`;
+
+suite("EdgeMotion", () => {
+  suite("EdgeMotion Cursor Movement", () => {
+    test("MoveToNextEdge moves cursor to the next island", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const testCases = [
+        { startLine: 1, endLine: 5, startChar: 0, endChar: 0 },
+        { startLine: 2, endLine: 4, startChar: 2, endChar: 2 },
+        { startLine: 3, endLine: 10, startChar: 4, endChar: 4 },
+      ];
+
+      for (const testCase of testCases) {
+        // Set initial cursor position
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+        await vscode.commands.executeCommand(
+          "vscode-edgemotion.MoveToNextEdge"
+        );
+        assert.strictEqual(
+          editor.selection.active.line,
+          testCase.endLine,
+          "Cursor did not move to the next edge"
+        );
+        assert.strictEqual(
+          editor.selection.active.character,
+          testCase.endChar,
+          "Cursor did not move to the next edge"
+        );
+      }
+    });
+
+    test("MoveToNextEdge moves cursor to the next shore", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const testCases = [
+        { startLine: 7, endLine: 10, startChar: 8, endChar: 8 },
+        { startLine: 13, endLine: 15, startChar: 0, endChar: 0 },
+      ];
+
+      for (const testCase of testCases) {
+        // Set initial cursor position
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+        await vscode.commands.executeCommand(
+          "vscode-edgemotion.MoveToNextEdge"
+        );
+        assert.strictEqual(
+          editor.selection.active.line,
+          testCase.endLine,
+          "Cursor did not move to the next edge"
+        );
+        assert.strictEqual(
+          editor.selection.active.character,
+          testCase.endChar,
+          "Cursor did not move to the next edge"
+        );
+      }
+    });
+
+    test("MoveToPreviousEdge moves cursor to the previous island", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const testCases = [
+        { startLine: 5, endLine: 1, startChar: 0, endChar: 0 },
+        { startLine: 4, endLine: 2, startChar: 2, endChar: 2 },
+        { startLine: 10, endLine: 3, startChar: 4, endChar: 4 },
+      ];
+
+      for (const testCase of testCases) {
+        // Set initial cursor position
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+        await vscode.commands.executeCommand(
+          "vscode-edgemotion.MoveToPreviousEdge"
+        );
+        assert.strictEqual(
+          editor.selection.active.line,
+          testCase.endLine,
+          "Cursor did not move to the previous edge"
+        );
+        assert.strictEqual(
+          editor.selection.active.character,
+          testCase.endChar,
+          "Cursor did not move to the previous edge"
+        );
+      }
+    });
+
+
+    test("MoveToNextEdge moves cursor to the previous shore", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const testCases = [
+        { startLine: 10, endLine: 7, startChar: 8, endChar: 8 },
+        { startLine: 15, endLine: 13, startChar: 0, endChar: 0 },
+      ];
+
+      for (const testCase of testCases) {
+        // Set initial cursor position
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+        await vscode.commands.executeCommand(
+          "vscode-edgemotion.MoveToPreviousEdge"
+        );
+        assert.strictEqual(
+          editor.selection.active.line,
+          testCase.endLine,
+          "Cursor did not move to the previous edge"
+        );
+        assert.strictEqual(
+          editor.selection.active.character,
+          testCase.endChar,
+          "Cursor did not move to the previous edge"
+        );
+      }
+    });
+
+
+    test("MoveToPreviousEdge moves cursor to the next island", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const testCases = [
+        { startLine: 5, endLine: 1, startChar: 0, endChar: 0 },
+        { startLine: 4, endLine: 2, startChar: 2, endChar: 2 },
+        { startLine: 3, endLine: 3, startChar: 4, endChar: 4 },
+      ];
+
+      for (const testCase of testCases) {
+        // Set initial cursor position
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+        await vscode.commands.executeCommand(
+          "vscode-edgemotion.MoveToPreviousEdge"
+        );
+        assert.strictEqual(
+          editor.selection.active.line,
+          testCase.endLine,
+          "Cursor did not move to the previous edge"
+        );
+        assert.strictEqual(
+          editor.selection.active.character,
+          testCase.endChar,
+          "Cursor did not move to the previous edge"
+        );
+      }
+    });
+
+    test("is island", async () => {
+      const document = await vscode.workspace.openTextDocument({
+        content: content,
+      });
+      const editor = await vscode.window.showTextDocument(document);
+
+      const islandTestCases = [
+        { startLine: 1, endLine: 1, startChar: 0, endChar: 0 },
+        { startLine: 2, endLine: 2, startChar: 2, endChar: 2 },
+        { startLine: 10, endLine: 10, startChar: 4, endChar: 4 },
+        { startLine: 10, endLine: 10, startChar: 5, endChar: 5 },
+      ];
+
+      const nonislandTestCases = [
+        { startLine: 2, endLine: 2, startChar: 0, endChar: 0 },
+        { startLine: 2, endLine: 2, startChar: 1, endChar: 1 },
+        { startLine: 7, endLine: 7, startChar: 1, endChar: 1 },
+        { startLine: 7, endLine: 7, startChar: 2, endChar: 2 },
+        { startLine: 10, endLine: 10, startChar: 6, endChar: 6 },
+        { startLine: 10, endLine: 10, startChar: 7, endChar: 7 },
+        { startLine: 10, endLine: 10, startChar: 10, endChar: 10 },
+      ];
+
+      const highlight = (
+        line: string,
+        startChar: number,
+      ): string => {
+        return `${line.slice(0, startChar)}→${line[startChar]}←${line.slice(startChar + 1)}`;
+      }
+
+      for (const testCase of islandTestCases) {
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+
+        assert.strictEqual(
+          edgeMotion.island(editor, testCase.startLine, testCase.startChar),
+          true,
+          `island did not return true at line ${testCase.startLine} and char ${testCase.startChar}.
+          ${highlight(document.lineAt(testCase.startLine).text, testCase.startChar)}
+          `
+        );
+      }
+
+      for (const testCase of nonislandTestCases) {
+        editor.selection = new vscode.Selection(
+          0,
+          0,
+          testCase.startLine,
+          testCase.startChar
+        );
+
+        assert.strictEqual(
+          edgeMotion.island(editor, testCase.startLine, testCase.startChar),
+          false,
+          `island did not return false at line ${testCase.startLine} and char ${testCase.startChar}`
+        );
+      }
+    });
+
+    test("getCharsAround", async () => {
+      const testCases = [
+        { line: "0123456789", col: 0, expected: ["", "0", "1"] },
+        { line: "0123456789", col: 1, expected: ["0", "1", "2"] },
+        { line: "0123456789", col: 2, expected: ["1", "2", "3"] },
+        { line: "0123456789", col: 9, expected: ["8", "9", ""] },
+        { line: "0123456789", col: 10, expected: [] },
+      ];
+
+      for (const testCase of testCases) {
+        const line = testCase.line;
+        const col = testCase.col;
+        const expected = testCase.expected;
+
+        const result = edgeMotion.getCharsAround(line, col);
+        assert.deepStrictEqual(
+          result,
+          expected,
+          `getCharsAround did not return expected result for line ${line} and col ${col}`
+        );
+      }
+    });
+
+  });
+});
